bb31a4503a2c550c14df57912611f38baf0f9f14
Alissonn1325 <alissonfbrito01@gmail.com>
2024-07-09 14:51:59 -0300

    Extra Pt.3

--- core/models/marca.py
+++ core/models/marca.py
@@ -6,4 +6,4 @@ class Marca(models.Model):
     nacionalidade = models.CharField(max_length=50, null=True)
 
     def __str__(self):
-        return f" Marca: {self.nome} | Nacionalidade: {self.nacionalidade} | ID: {self.id}"
+        return f" Marca: {self.nome.upper()} | Nacionalidade: {self.nacionalidade} | ID: {self.id}"
